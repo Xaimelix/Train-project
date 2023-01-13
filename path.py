7c95803e4c2c316e3bf5d3926f200f864c143f01
Xami <Xaimfolk355@yandex.ru>
2023-01-13 14:53:16 +1000

    find way func

---
+++ path.py
@@ -0,0 +1,28 @@
+import heapq
+
+graph = {'A': [(1, 'B')], 'B': [(3, 'C'), (2, 'E'), (1, 'A')], 'C': [(2, 'D'), (3, 'B')],
+         'D': [(2, 'E'), (2, 'C')], 'E': [(3, 'F'), (2, 'B')], 'F': [(3, 'E')]}
+
+
+def dijkstra(start, end, graph):
+    queue = []
+    heapq.heappush(queue, (0, start))
+    cost_visited = {start: 0}
+    visited = {start: None}
+
+    while queue:
+        cur_cost, cur_node = heapq.heappop(queue)
+        if cur_node == end:
+            break
+
+        for next_node in graph[cur_node]:
+            neighbor_cost, neighbor_node = next_node
+            new_cost = cost_visited[cur_node] + neighbor_cost
+
+            if neighbor_node not in cost_visited or new_cost < cost_visited[neighbor_node]:
+                heapq.heappush(queue, (new_cost, neighbor_node))
+                cost_visited[neighbor_node] = new_cost
+                visited[neighbor_node] = cur_node
+
+    return visited
+
